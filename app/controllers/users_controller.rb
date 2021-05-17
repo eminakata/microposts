0b4d290b40400782077faac12f8f6cb4c36d5529
emi.nakata <19bn022h@rikkyo.ac.jp>
2021-05-17 12:46:12 +0000

    kadai-microposts3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,7 @@ class UsersController < ApplicationController
   def likes
     @user = User.find(params[:id])
     @favoritings = @user.favoritings.page(params[:page])
+    counts(@user)
   end
   
   private
